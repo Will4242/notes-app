1fae8447186a0b1187c44c6db114ecbc72781d7f
Will <20088880@mail.wit.ie>
2022-03-14 09:26:14 +0000

    Set up persistence XML.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,5 +32,8 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("io.github.microutils:kotlin-logging:2.1.21")
     testImplementation("org.slf4j:slf4j-simple:2.0.0-alpha6")
+    //For Streaming to XML and JSON
+    implementation("com.thoughtworks.xstream:xstream:1.4.18")
+    implementation("org.codehaus.jettison:jettison:1.4.1")
 
 }
