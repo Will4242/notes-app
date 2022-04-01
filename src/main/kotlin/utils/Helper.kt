b3e19f0e5e30ac95cd8c1a55451f5358de197d30
Will <20088880@mail.wit.ie>
2022-04-01 11:27:57 +0100

    Junit testing for status validation.

--- src/main/kotlin/utils/Helper.kt
+++ src/main/kotlin/utils/Helper.kt
@@ -3,6 +3,7 @@ package utils
 object Helper {
     @JvmStatic
     var categories = listOf("work", "college", "home", "sport", "holidays")
+    var statuses = listOf("todo", "doing", "done")
     // utility method to determine if an index is valid in a list.
     @JvmStatic
     fun isValidListIndex(index: Int, list: List<Any>): Boolean {
@@ -14,12 +15,10 @@ object Helper {
     }
     @JvmStatic
     fun isValidCategory(category: String): Boolean {
-
         return categories.contains(category.lowercase())
     }
     @JvmStatic
     fun isValidStatus(status: String): Boolean {
-        var statuses = listOf("todo", "doing", "done")
         return statuses.contains(status.lowercase())
     }
 }
